6f4baa8fb599f44488656235fd454e6c94dd3da0
Ben Dover <ron.stuchevsky@gmail.com>
2024-12-23 18:46:18 -0500

    chapter 6

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,3 @@
+export default function invoicesPage(){
+    return(<p>invoices page</p>)
+}
---
+++ app/ui/font/fonts.ts
@@ -0,0 +1,4 @@
+import { Playfair_Display, Lusitana } from "next/font/google";
+
+export const playfair = Playfair_Display({ subsets: ["latin"] });
+export const lusitana = Lusitana({ weight: ["400", "700"] ,subsets: ["latin"] });
